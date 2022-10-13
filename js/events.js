0eb7485fa4e5803e3ca49889c32c0c24132d317c
Rhenald <renaldkarel@gmail.com>
2022-10-13 09:53:06 +0700

    style: format code

--- js/events.js
+++ js/events.js
@@ -1,27 +1,24 @@
-import {
-  todos,
-  RENDER_EVENT,
-} from "./constant.js";
+import { todos, RENDER_EVENT } from './constant.js';
 
 import {
-  generateId,
-  generateTodoObject,
-  saveData,
-  findTodo,
-  findTodoIndex,
-} from "./utils.js";
+	generateId,
+	generateTodoObject,
+	saveData,
+	findTodo,
+	findTodoIndex,
+} from './utils.js';
 
 export function addTodo() {
 	const textTodo = document.getElementById('title').value;
 	const timestamp = document.getElementById('date').value;
-  const clock = document.getElementById('time').value;
+	const clock = document.getElementById('time').value;
 
 	const generatedID = generateId();
 	const todoObject = generateTodoObject(
 		generatedID,
 		textTodo,
 		timestamp,
-    clock,
+		clock,
 		false
 	);
 	todos.push(todoObject);
@@ -39,7 +36,6 @@ export function addTaskToCompleted(todoId) {
 	document.dispatchEvent(new Event(RENDER_EVENT));
 	saveData();
 }
-
 
 export function removeTaskFromCompleted(todoId) {
 	const todoTarget = findTodoIndex(todoId);
